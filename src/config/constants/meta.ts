b40e086512ce3357043071f7f853ced3b45b7221
panoptisDev <panoptis.network@gmail.com>
2023-03-07 01:32:03 +0100

    update name

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -3,10 +3,10 @@ import { ContextApi } from '@pancakeswap/localization'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'PikachuSwap',
+  title: 'WhaleHarvest',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PikachuSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pikachuswap.finance/images/hero.png',
+    'A popular AMM on BSC by user count! Earn LAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by WhaleHarvest), NFTs, and more, on a platform you can trust.',
+  image: 'https://panoptisdev.github.io/whaleharvest.github.io/lake_96.png',
 }
 
 interface PathList {
@@ -46,7 +46,7 @@ const getPathList = (t: ContextApi['t']): PathList => {
       '/pancake-squad': { basePath: true, title: t('Pancake Squad') },
       '/pottery': { basePath: true, title: t('Pottery') },
     },
-    defaultTitleSuffix: t('PikachuSwap'),
+    defaultTitleSuffix: t('WhaleHarvest'),
   }
 }
 
